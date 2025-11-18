e8ea3dce89a108bbdd2310333fcb9ee20447e4cf
Do Ngo <bahuynh619@gmail.com>
2025-11-18 13:59:09 +0700

    Update Challenges.py

--- Challenges.py
+++ Challenges.py
@@ -7,7 +7,7 @@ tip_amount = bill_amount * tip_rate
 total_amount = bill_amount + tip_amount
 print(f"Tổng số tiền phải trả bao gồm tiền tip là : {total_amount}") 
 
-//Tính tổng tiền bao gồm luôn tiền tip
+#Tính tổng tiền bao gồm luôn tiền tip
 
 
 #03
