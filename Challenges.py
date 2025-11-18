5bc645f59cba6bc16e882797500c169d56827f2c
Do Ngo <bahuynh619@gmail.com>
2025-11-18 13:56:33 +0700

    Fix formatting of print statement in Challenges.py

---
+++ Challenges.py
@@ -0,0 +1,12 @@
+
+#02
+
+bill_amount = float(input("Số tiền hóa đơn (VNĐ): "))
+tip_rate = float(input("Tỷ lệ tip (%, vd: 10): ")) / 100
+tip_amount = bill_amount * tip_rate
+total_amount = bill_amount + tip_amount
+print(f"Tổng số tiền phải trả bao gồm tiền tip là : {total_amount}")
+
+
+#03
+
