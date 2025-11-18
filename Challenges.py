7b1e95d818cf9f3d6dd988555cebf35a94ecbb17
Do Ngo <bahuynh619@gmail.com>
2025-11-18 13:58:29 +0700

    Update Challenges.py

--- Challenges.py
+++ Challenges.py
@@ -5,7 +5,9 @@ bill_amount = float(input("Số tiền hóa đơn (VNĐ): "))
 tip_rate = float(input("Tỷ lệ tip (%, vd: 10): ")) / 100
 tip_amount = bill_amount * tip_rate
 total_amount = bill_amount + tip_amount
-print(f"Tổng số tiền phải trả bao gồm tiền tip là : {total_amount}") //Tính tổng tiền bao gồm luôn tiền tip
+print(f"Tổng số tiền phải trả bao gồm tiền tip là : {total_amount}") 
+
+//Tính tổng tiền bao gồm luôn tiền tip
 
 
 #03
