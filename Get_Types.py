14c916e297facad08f03246a47035afe78a82789
Do Ngo <bahuynh619@gmail.com>
2025-11-18 16:59:25 +0700

    Create Get_Types.py
    Add user input for age, height, and weight to calculate BMI.

---
+++ Get_Types.py
@@ -0,0 +1,11 @@
+age = int(input("Tuổi của bạn: "))
+next_year = age + 1
+
+# Số thập phân
+height = float(input("Chiều cao (m): "))
+weight = float(input("Cân nặng (kg): "))
+bmi = weight / (height ** 2)
+print(f"Năm sau bạn sẽ {next_year} tuổi")
+print(f"BMI của bạn: {bmi:.2f}")
+
+#Nhận Text_String
